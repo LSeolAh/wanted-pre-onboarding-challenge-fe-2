154df385979f2c8e2ee45a31bd863f002d7f114e
LSeolAh <sualah0417@gmail.com>
2022-08-30 20:42:49 +0900

    수행 과제

---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * @param {Number} 아이디 - Todo의 아이디
+ * @param {String} 내용 - Todo의 내용
+ * @param {Boolean} 완료여부 - Todo의 완료 여부
+ * @param {String} 카테고리 - Todo의 카테고리
+ * @param {String} 태그 - Todo의 태그
+ */
+const Todo = (아이디, 내용, 완료여부, 카테고리, 태그) => {};
+
+
+/**
+ * Todo 추가하기
+ * @param {String} 내용 - Todo에 추가하려는 내용
+ * @param {String} 카테고리 - Todo에 추가하려는 카테고리
+ * @param {String} 태그 - Todo에 추가하려는 태그
+ */
+const CREATE = (내용, 카테고리, 태그) => {};
+
+/**
+ * Todo 조회하기
+ * @param {Number} 아이디 - 조회하려는 Todo의 아이디
+ */
+const READ = (아이디) => {};
+
+/**
+ * Todo 수정하기
+ * @param {Number} 아이디 - 수정하려는 Todo의 아이디
+ * @param {String} 내용 - Todo의 내용
+ * @param {Boolean} 완료여부 - Todo의 완료 여부
+ * @param {String} 카테고리 - Todo의 카테고리
+ * @param {String} 태그 - Todo의 태그
+ */
+function UPDATE(아이디, 내용, 완료여부, 카테고리, 태그) {Todo()}
+
+/**
+ * Todo 삭제하기
+ * @param {Number} 아이디 - 삭제하려는 Todo의 아이디
+ */
+const DELETE = (아이디) => {};
